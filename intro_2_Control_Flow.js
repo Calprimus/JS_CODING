195ee8ee452d78d186d470c5dba0a70201e75783
Calprimus <fulvio.famularo@hotmail.com>
2018-05-15 20:48:55 +0200

    CA 2 -2/11

--- intro_2_Control_Flow.js
+++ intro_2_Control_Flow.js
@@ -1,12 +1,28 @@
-let userName = 'Fulvio';
-let knowsJavaScript = true;
+// >>>> 1/11
+// let userName = 'Fulvio';
+// let knowsJavaScript = true;
 
-if (knowsJavaScript && userName) {
-    console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
-} else if (knowsJavaScript) {
-    console.log('Great! Get ready to practice your JavaScript!');
-} else if (userName) {
-    console.log('Great, ' + userName + '! Get ready to learn something new!');
+// if (knowsJavaScript && userName) {
+//     console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+// } else if (knowsJavaScript) {
+//     console.log('Great! Get ready to practice your JavaScript!');
+// } else if (userName) {
+//     console.log('Great, ' + userName + '! Get ready to learn something new!');
+// } else {
+//     console.log('Great! Get ready to learn something new!');
+// }
+
+// >>>> 2/11
+// let needsCoffee = true;
+// if (needsCoffee === true) {
+//     console.log('Finding coffee');
+// } else {
+//     console.log('Keep on keeping on!');
+// }
+
+let isSoccerFan = true;
+if (isSoccerFan === true) {
+    console.log('Rawr');
 } else {
-    console.log('Great! Get ready to learn something new!');
+    console.log('nvm');
 }
