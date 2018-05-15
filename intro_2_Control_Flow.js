9b2cfbc21240f4379301cf76bbb55ef66099e627
Calprimus <fulvio.famularo@hotmail.com>
2018-05-15 21:13:00 +0200

    CA 2 4/11

--- intro_2_Control_Flow.js
+++ intro_2_Control_Flow.js
@@ -46,3 +46,21 @@
 // }
 
 // >>>> 4/11
+// let wordCount = 10;
+
+// if (wordCount) {
+//     console.log("Great! You've started your work!");
+// } else {
+//     console.log('Better get to work!');
+// }
+
+
+// let favoritePhrase = 'Oh Dear';
+
+// if (!favoritePhrase) {
+//     console.log("This string doesn't seem to be empty.");
+// } else {
+//     console.log('This string is definitely empty.');
+// }   
+
+// >>>> 5/11
