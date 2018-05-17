5bedddea4a9a77fdf062d268a9d82656501d7240
Calprimus <fulvio.famularo@hotmail.com>
2018-05-17 19:46:22 +0200

    CA 2 7/11

--- intro_2_Control_Flow.js
+++ intro_2_Control_Flow.js
@@ -73,12 +73,37 @@
 // }
 
 // >>>> 6/11
+// var moonPhase = "full"
+
+// if (moonPhase === "full") {
+//     console.log("Howl!")
+// } else {
+//     console.log("I swear I am not a werewolf")
+// }
+
+// >>>> 7/11
+// let stopLight = 'green';
+
+// if (stopLight === 'red') {
+//     console.log('Stop');
+// } else if (stopLight === 'yellow') {
+//     console.log('Slow down');
+// } else if (stopLight === 'green') {
+//     console.log('Go!');
+// } else {
+//     console.log('Caution, unknown!');
+// }
+
 var moonPhase = "full"
 
 if (moonPhase === "full") {
     console.log("Howl!")
+} else if (moonPhase === "mostly full") {
+    console.log("Arms and legs are getting hairier")
+} else if (moonPhase === "mostly new") {
+    console.log("Back on two feet")
 } else {
-    console.log("I swear I am not a werewolf")
+    console.log("Invalid moon phase")
 }
 
-// >>>> 7/11
+// >>>> 8/11
