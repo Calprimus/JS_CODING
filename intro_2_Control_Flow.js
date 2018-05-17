6f2b002f4cd07f706a721e7eb084f92adb5d33e2
Calprimus <fulvio.famularo@hotmail.com>
2018-05-17 22:02:53 +0200

    CA 2 10/11

--- intro_2_Control_Flow.js
+++ intro_2_Control_Flow.js
@@ -144,24 +144,72 @@
 //         break;
 // }
 
-var moonPhase = "full"
+// var moonPhase = "full"
 
-switch (moonPhase) {
-    case 'full':
-        console.log('Howl!');
-        break;
+// switch (moonPhase) {
+//     case 'full':
+//         console.log('Howl!');
+//         break;
 
-    case 'mostly full':
-        console.log('Arms and legs are getting hairier');
-        break;
+//     case 'mostly full':
+//         console.log('Arms and legs are getting hairier');
+//         break;
 
-    case 'mostly new':
-        console.log('Back on two feet');
-        break;
+//     case 'mostly new':
+//         console.log('Back on two feet');
+//         break;
 
-    default:
-        console.log('Invalid moon Phase');
-        break;
-}
+//     default:
+//         console.log('Invalid moon Phase');
+//         break;
+// }
 
 // >>>> 10/11
+// // -----------------------------------------------------------------------------------
+// let isNightTime = true;
+
+// if (isNightTime) {
+//     console.log('Turn on the lights!');
+// } else {
+//     console.log('Turn off the lights!');
+// }
+
+// isNightTime ? console.log('Turn on the lights!') : console.log('Turn off the lights!');
+// // --------------------------------------------------------------------------------------
+// age >= 16 ? console.log('You are old enough to drive in the United States!') : console.log('You are not old enough to drive in the United States!');
+// // --------------------------------------------------------------------------------------
+
+let isLocked = false;
+
+if (isLocked) {
+    console.log('You will need a key to open the door.');
+} else {
+    console.log('You will not need a key to open the door.');
+}
+
+isLocked ? console.log('You will need a key to open the door.') :
+    console.log('You will not need a key to open the door.');
+
+
+
+let isCorrect = true;
+
+if (isCorrect) {
+    console.log('Correct!');
+} else {
+    console.log('Incorrect!');
+}
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+    console.log('I love that!');
+} else {
+    console.log("I don't love that!");
+}
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
