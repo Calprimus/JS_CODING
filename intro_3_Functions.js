3c0d0294264ef10109d42e32f7a735bd1b51e5df
Calprimus <fulvio.famularo@hotmail.com>
2018-05-18 17:35:12 +0200

    F 1/10

---
+++ intro_3_Functions.js
@@ -0,0 +1,21 @@
+// >>>> 1/10
+
+let calculatorIsOn = false;
+
+const pressPowerButton = () => {
+    if (calculatorIsOn) {
+        console.log('Calculator turning off.');
+        calculatorIsOn = false;
+    } else {
+        console.log('Calculator turning on.');
+        calculatorIsOn = true;
+    }
+};
+
+pressPowerButton();
+// Output: Calculator turning on.
+
+pressPowerButton();
+// Output: Calculator turning off.
+
+// >>>> 2/10
