11c04efe70cf84ce69e8ab79caeefcf3574be233
Calprimus <fulvio.famularo@hotmail.com>
2018-05-18 21:04:19 +0200

    F 9/10

--- intro_3_Functions.js
+++ intro_3_Functions.js
@@ -157,14 +157,37 @@
 
 // isGreaterThan(4, 8)
 
-const isGreaterThan = (numberOne, numberTwo) => {
-    if (numberOne > numberTwo) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// const isGreaterThan = (numberOne, numberTwo) => {
+//     if (numberOne > numberTwo) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-isGreaterThan(4, 8)
+// isGreaterThan(4, 8)
 
 // >>>> 9/10
+const multiplyByNineFifths = (celsius) => {
+    return celsius * (9 / 5);
+};
+
+const getFahrenheit = (celsius) => {
+    return multiplyByNineFifths(celsius) + 32;
+};
+
+console.log('The temperature is ' + getFahrenheit(15) + '°F');
+
+// -------------------------------------------------------------------------
+
+const multiplyByNineFifths = celsius => celsius * (9 / 5);
+
+const getFahrenheit = celsius => multiplyByNineFifths(celsius) + 32;
+
+console.log('The temperature is ' + getFahrenheit(15) + '°F');
+
+// ------------------------------------------------------------------------
+
+const volumeOfSphere = diameter => (1 / 6) * Math.PI * diameter * diameter * diameter;
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
