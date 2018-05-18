10972aba2ffb8a05769fb3a3ad0f7fcd1268de9f
Calprimus <fulvio.famularo@hotmail.com>
2018-05-18 17:40:16 +0200

    F 2/10

--- intro_3_Functions.js
+++ intro_3_Functions.js
@@ -1,21 +1,28 @@
 // >>>> 1/10
 
-let calculatorIsOn = false;
+// let calculatorIsOn = false;
 
-const pressPowerButton = () => {
-    if (calculatorIsOn) {
-        console.log('Calculator turning off.');
-        calculatorIsOn = false;
-    } else {
-        console.log('Calculator turning on.');
-        calculatorIsOn = true;
-    }
-};
+// const pressPowerButton = () => {
+//     if (calculatorIsOn) {
+//         console.log('Calculator turning off.');
+//         calculatorIsOn = false;
+//     } else {
+//         console.log('Calculator turning on.');
+//         calculatorIsOn = true;
+//     }
+// };
 
-pressPowerButton();
-// Output: Calculator turning on.
+// pressPowerButton();
+// // Output: Calculator turning on.
 
-pressPowerButton();
-// Output: Calculator turning off.
+// pressPowerButton();
+// // Output: Calculator turning off.
 
 // >>>> 2/10
+const takeOrder = () => {
+    console.log('Order: pizza')
+}
+
+takeOrder()
+
+// >>>> 3/10
