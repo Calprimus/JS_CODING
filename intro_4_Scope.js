4777399f25529a7b03c46a4a08dc630feb65f30f
Calprimus <fulvio.famularo@hotmail.com>
2018-05-19 18:28:30 +0200

    S 3/7

--- intro_4_Scope.js
+++ intro_4_Scope.js
@@ -7,13 +7,29 @@
 
 // console.log(colorOfSky()); // blue
 
+// const satellite = 'The Moon'
+// const galaxy = 'The Milky Way'
+
+// let stars = 'North Star'
+
+// const myNightSky = () => {
+//     return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+// }
+
+// console.log(myNightSky())
+
+// >>>> 3/7
 const satellite = 'The Moon'
 const galaxy = 'The Milky Way'
 
 let stars = 'North Star'
 
 const myNightSky = () => {
+    stars = 'Sirius';
     return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 }
 
 console.log(myNightSky())
+console.log(stars)
+
+// >>>> 4/7
