2deb0cea2b2e5857e74cd9cbc1edd07cfbb43005
Calprimus <fulvio.famularo@hotmail.com>
2018-05-19 18:17:18 +0200

    S 2/7

---
+++ intro_4_Scope.js
@@ -0,0 +1,19 @@
+// >>>> 2/7
+// const color = 'blue'
+
+// const colorOfSky = () => {
+//     return color; // blue 
+// };
+
+// console.log(colorOfSky()); // blue
+
+const satellite = 'The Moon'
+const galaxy = 'The Milky Way'
+
+let stars = 'North Star'
+
+const myNightSky = () => {
+    return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+}
+
+console.log(myNightSky())
