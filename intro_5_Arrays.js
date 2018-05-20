41271c4aeabbea8f19fed5de0a9d153e34ab9a0d
Calprimus <fulvio.famularo@hotmail.com>
2018-05-20 20:01:56 +0200

    Array 9/9

--- intro_5_Arrays.js
+++ intro_5_Arrays.js
@@ -79,17 +79,46 @@
 // console.log(groceryList);
 
 // >>>> 8/9
-let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+// let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 
-const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+// const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
 
-condiments.push('Mayo');
-console.log(condiments);
+// condiments.push('Mayo');
+// console.log(condiments);
 
-condiments = ['a'];
+// condiments = ['a'];
 
-utensils.pop();
-console.log(utensils);
-// utensils = ['b'];
+// utensils.pop();
+// console.log(utensils);
+// // utensils = ['b'];
 
-// >>>>
+// >>>> 9/9
+// Review Arrays
+// Nice work! In this lesson, we learned these concepts regarding arrays:
+
+// Arrays are lists and are a way to store data in JavaScript.
+
+// Arrays are created with brackets [].
+
+// Each item inside of an array is at a numbered position, starting at 0.
+
+// We can access one item in an array using its numbered position, 
+// with syntax like: myArray[0].
+
+// We can also change an item in an array using its numbered position, 
+// with syntax like myArray[0] = "new string";
+
+// Arrays have a length property, which allows you to see how many items 
+// are in an array.
+
+// Arrays have their own methods, including .push() and .pop(), 
+// which add and remove items from an array, respectively.
+
+// Arrays have many other methods that perform different functions, 
+// such as .slice() and .shift(). You can read the documentation 
+// for any array method on the Mozilla Developer Network website.
+
+// Variables that contain arrays can be declared with let or const. 
+// Even when declared with const, arrays are still mutable; 
+// they can be changed. However, a variable declared with const 
+// cannot be reassigned.
