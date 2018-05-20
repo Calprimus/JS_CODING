c903d73d44271e0099c8f47c622008f7a8562507
Calprimus <fulvio.famularo@hotmail.com>
2018-05-20 19:37:31 +0200

    Array 6/9

--- intro_5_Arrays.js
+++ intro_5_Arrays.js
@@ -38,6 +38,17 @@
 // console.log(newYearsResolutions[3]);
 
 // >>>> 5/9
+// let newYearsResolutions = ['a', 'b', 'c'];
+// newYearsResolutions[1] = 'Learn a new language';
+
+// console.log(newYearsResolutions)
+// let listItem = newYearsResolutions[0];
+// console.log(listItem);
+// console.log(newYearsResolutions[2]);
+// console.log(newYearsResolutions[3]);
+// console.log(newYearsResolutions.length);
+
+// >>>> 6/9
 let newYearsResolutions = ['a', 'b', 'c'];
 newYearsResolutions[1] = 'Learn a new language';
 
@@ -48,4 +59,9 @@ console.log(newYearsResolutions[2]);
 console.log(newYearsResolutions[3]);
 console.log(newYearsResolutions.length);
 
-// >>>> 6/9
+newYearsResolutions.push('d', 'e');
+console.log(newYearsResolutions);
+newYearsResolutions.pop();
+console.log(newYearsResolutions);
+
+// >>>> 7/9
