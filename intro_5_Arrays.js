ce93b25b888c4f355dd6eba8f5538bdba14afcac
Calprimus <fulvio.famularo@hotmail.com>
2018-05-20 18:29:27 +0200

    Array 2/9

--- intro_5_Arrays.js
+++ intro_5_Arrays.js
@@ -1,4 +1,11 @@
 // >>>> 1/9
-let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
+// let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
 
-console.log(bucketList);
+// console.log(bucketList);
+
+// >>>> 2/9
+let newYearsResolutions = ['a', 'b', 'c'];
+
+console.log(newYearsResolutions)
+
+// >>>> 3/9
