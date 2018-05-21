eb36bc0872024a71b0bfe8c12f799e7c71870269
Calprimus <fulvio.famularo@hotmail.com>
2018-05-21 18:18:56 +0200

    Loop 8/8

--- intro_6_Loops.js
+++ intro_6_Loops.js
@@ -62,15 +62,39 @@
 // console.log('found a ' + currentCard);
 
 // >>>> 7/8
-let flag = true;
-let counter = 0;
+// let flag = true;
+// let counter = 0;
 
-while (flag === true) {
-    console.log(counter);
-    counter += 1;
-    if (counter === 37) {
-        break;
-    }
-}
+// while (flag === true) {
+//     console.log(counter);
+//     counter += 1;
+//     if (counter === 37) {
+//         break;
+//     }
+// }
 
 // >>>> 8/8
+// LOOPS
+// Review: Loops
+// Great job! In this lesson, we learned how to write less repetitive code with loops.
+
+// for loops allow us to repeat a block of code a known amount of times.
+
+// We can use a for loop inside another for loop to compare two lists.
+
+// while loops are for looping over a code block an unknown amount of times.
+
+// Infinite loops occur when stop conditions are never met.
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+console.log(groceryList);
+
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+
+
+console.log(groceryList.slice(1, 4));
