57c4f55926ebf32e325098823c2fad24d4526863
Calprimus <fulvio.famularo@hotmail.com>
2018-05-21 16:04:02 +0200

    Loop 1/8

---
+++ intro_6_Loops.js
@@ -0,0 +1,8 @@
+// >>>> 1/8
+let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+
+for (let i = 0; i < cookies.length; i++) {
+    console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
+}
+
+// >>>> 2/8
