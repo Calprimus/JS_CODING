56320e752c68dbca2664a432f2027412c88963ff
Calprimus <fulvio.famularo@hotmail.com>
2018-05-22 18:25:04 +0200

    Iter 2/7

--- intro_7_Iterators.js
+++ intro_7_Iterators.js
@@ -22,3 +22,32 @@
 // console.log(onlyNumbers);
 
 // >>>> 2/7
+// let groceries = ['whole wheat flour', 'brown sugar', 'salt', 'cranberries', 'walnuts'];
+
+// groceries.forEach(function(groceryItem) {
+//     console.log(' - ' + groceryItem);
+// });
+
+// groceries.forEach(groceryItem => console.log(' - ' + groceryItem));
+
+// // -----------------------------------------------------------------------
+
+// // There are three important things to know about the .forEach() method.
+
+// // 1) It is an array method. It must be called upon an array.
+// // 2) Any changes to the iterated array value won't be updated in the original array.
+// // 3) The return value is undefined.
+
+// // -----------------------------------------------------------------------
+
+// let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// // Iterate over fruits below
+
+// fruits.forEach(function(fruit) {
+//     console.log('I want to eat a ' + fruit)
+// });
+
+// fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+
+// >>>> 3/7
