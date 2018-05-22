29f212b33ce3353f78cc9a78e96ba79c4a055e90
Calprimus <fulvio.famularo@hotmail.com>
2018-05-22 20:41:52 +0200

    Iter 3/7

--- intro_7_Iterators.js
+++ intro_7_Iterators.js
@@ -51,3 +51,34 @@
 // fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
 
 // >>>> 3/7
+// let numbers = [1, 2, 3, 4, 5]; 
+
+// let bigNumbers = numbers.map(function(number) {
+//   return number * 10;
+// });
+// // ------------------------------------------------------
+// let numbers = [1, 2, 3, 4, 5]; 
+
+// let bigNumbers = numbers.map(numbers => numbers * 10);
+
+// let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// // Create the secretMessage array below
+// let secretMessage = animals.map(function(animals){
+//   return animals[0];
+// })
+
+// console.log(secretMessage.join(''));
+
+// let bigNumbers = [100, 200, 300, 400, 500];
+
+// // Create the smallNumbers array below
+// let smallNumbers = bigNumbers.map(function(bigNumbers){
+//   return bigNumbers / 100;
+// });
+
+// // -------- Refactor the method call from Step 1 using arrow function syntax.
+// let secretMessage = animals.map(animals => animals[0]);
+// console.log(secretMessage.join(''));
+
+// >>>> 4/7
