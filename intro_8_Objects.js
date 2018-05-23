b28cbc8af2b591dce8e0d4623107123d15b70f9b
Calprimus <fulvio.famularo@hotmail.com>
2018-05-23 17:21:49 +0200

    Obj 3/15

--- intro_8_Objects.js
+++ intro_8_Objects.js
@@ -7,16 +7,34 @@
 // Create object getter and setter methods
 
 // >>>> 2/15
+// let restaurant = {
+//     name: 'Italian Bistro',
+//     seatingCapacity: 120,
+//     hasDineInSpecial: true,
+//     entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine Pesto']
+// };
+
+// let person = {
+//     name: 'Fulvio',
+//     age: 53
+// };
+
+// >>>> 3/15
 let restaurant = {
     name: 'Italian Bistro',
     seatingCapacity: 120,
     hasDineInSpecial: true,
-    entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine Pesto']
+    entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine pesto']
 };
+
+console.log(restaurant.entrees);
 
 let person = {
     name: 'Fulvio',
     age: 53
 };
 
-// >>>> 3/15
+console.log(person.name)
+console.log(person.age)
+
+//   >>>> 4/15
