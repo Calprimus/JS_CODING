6f8b5918349a76bed0aedf9071c01cb6fd5d682f
Calprimus <fulvio.famularo@hotmail.com>
2018-05-23 16:51:24 +0200

    Obj 1/15

---
+++ intro_8_Objects.js
@@ -0,0 +1,9 @@
+// >>>> 1/15
+// Throughout this lesson, you will learn how to:
+
+// Represent real-world objects in JavaScript
+// Access object properties
+// Access object methods
+// Create object getter and setter methods
+
+// >>>> 2/15
