a5f5a596c593e48cc50eb4bfa8a3a303ce9808c1
Calprimus <fulvio.famularo@hotmail.com>
2018-05-24 21:05:52 +0200

    Obj 14/15

--- intro_8_Objects.js
+++ intro_8_Objects.js
@@ -388,3 +388,57 @@
 // restaurant.seatingCapacity = 150; //150 is valid input.
 
 // >>>> 14/15
+// let restaurant = {
+//     _name: 'Italian Bistro',
+//     _seatingCapacity: 120,
+//     _hasDineInSpecial: true,
+//     _entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine pesto'],
+
+//     set seatingCapacity(newCapacity) {
+//         if (typeof newCapacity === 'number') {
+//             this._seatingCapacity = newCapacity;
+//         } else {
+//             console.log(`Change ${newCapacity} to a number.`)
+//         }
+//     },
+
+//     get seatingCapacity() {
+//         console.log(`There are ${this._seatingCapacity} seats at Italian Bistro.`);
+//         return this._seatingCapacity;
+//     }
+// }
+// ------------------------------
+// In the example above, the getter method (get seatingCapacity()) 
+// logs something to the console and returns the value saved to _seatingCapacity. 
+// We call the getter method the same way we would access a property without a method:
+// ------------------------------
+// restaurant.seatingCapacity = 150;
+// const seats = restaurant.seatingCapacity;
+// ------------------------------
+// In this example we set the seatingCapacity to 150, 
+// then call the getter method using restaurant.seatingCapacity 
+// and save the result to a variable called seats. 
+// The getter will also log the following line of code to the console:
+// >>There are 150 seats at Italian Bistro.
+// --------------------------------
+
+let person = {
+    _name: 'Lu Xun',
+    _age: 137,
+
+    set age(val) {
+        if (typeof val === 'number') {
+            this._age = val;
+        } else {
+            return 'Invalid input'
+        }
+    },
+
+    get age() {
+        console.log(`${this._name} is ${this._age} years old`)
+    }
+};
+
+console.log(person.age)
+
+// >>>> 15/15
