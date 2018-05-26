7ed2c5ba34299f5cd94b5357ae9da45a570cfcd1
Calprimus <fulvio.famularo@hotmail.com>
2018-05-26 21:32:03 +0200

    Class 10/12

--- intro_9_Classes.js
+++ intro_9_Classes.js
@@ -315,6 +315,40 @@
 // const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics'])
 
 // >>>> 9/12
+// class HospitalEmployee {
+//     constructor(name) {
+//         this._name = name;
+//         this._remainingVacationDays = 20;
+//     }
+
+//     get name() {
+//         return this._name;
+//     }
+
+//     get remainingVacationDays() {
+//         return this._remainingVacationDays;
+//     }
+
+//     takeVacationDays(daysOff) {
+//         return this._remainingVacationDays = this._remainingVacationDays - daysOff;
+//     }
+
+// }
+
+// class Nurse extends HospitalEmployee {
+//     constructor(name, certifications) {
+//         super(name);
+//         this._certifications = certifications;
+//     }
+// }
+
+// const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics'])
+
+// nurseOlynyk.takeVacationDays(5);
+// console.log(nurseOlynyk.remainingVacationDays);
+
+// >>>> 10/12
+
 class HospitalEmployee {
     constructor(name) {
         this._name = name;
@@ -330,9 +364,8 @@ class HospitalEmployee {
     }
 
     takeVacationDays(daysOff) {
-        return this._remainingVacationDays = this._remainingVacationDays - daysOff;
+        this._remainingVacationDays -= daysOff;
     }
-
 }
 
 class Nurse extends HospitalEmployee {
@@ -340,11 +373,22 @@ class Nurse extends HospitalEmployee {
         super(name);
         this._certifications = certifications;
     }
+
+    get certifications() {
+        return this._certifications;
+    }
+
+    addCertification(newCertification) {
+        this.certifications.push(newCertification)
+    }
+
 }
 
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics'])
-
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
 nurseOlynyk.takeVacationDays(5);
 console.log(nurseOlynyk.remainingVacationDays);
 
-// >>>> 10/12
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+// >>>> 11/12
