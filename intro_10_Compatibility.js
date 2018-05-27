2a1ad11fe76bee484b0cd79b82e4b03142a8d5c2
Calprimus <fulvio.famularo@hotmail.com>
2018-05-27 20:47:08 +0200

    Comp 2/11

--- intro_10_Compatibility.js
+++ intro_10_Compatibility.js
@@ -1,12 +1,13 @@
 // >>>> 1/11
+// var pasta = "Spaghetti"; // ES5 syntax
 
-var pasta = "Spaghetti"; // ES5 syntax
+// const meat = "Pancetta"; // ES6 syntax
 
-const meat = "Pancetta"; // ES6 syntax
+// let sauce = "Eggs and cheese"; // ES6 syntax
 
-let sauce = "Eggs and cheese"; // ES6 syntax
-
-// Template literals, like the one below, were introduced in ES6
-const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+// // Template literals, like the one below, were introduced in ES6
+// const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
 
 // >>>> 2/11
+// nothing
+// caniuse.com
