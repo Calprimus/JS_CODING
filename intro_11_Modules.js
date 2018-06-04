1aa8f8306eea33bca934c6f99709e2a67ef76df3
Calprimus <fulvio.famularo@hotmail.com>
2018-06-04 21:28:04 +0200

    Modules 14/15

--- intro_11_Modules.js
+++ intro_11_Modules.js
@@ -339,3 +339,33 @@
 // export { availableAirplanes as aircrafts, flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs };
 
 // >>>> 14/15
+// import { specialty, isVegetarian, isLowSodium } from './menu';
+
+// import GlutenFree from './menu';
+
+// import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+// import meetsSpeedRangeRequirements from './airplane';
+
+// function displayFuelCapacity() {
+//   availableAirplanes.forEach(function(element) {
+//     console.log('Fuel Capacity of ' + element['name'] + ': ' + element['fuelCapacity']);
+//   });
+// };
+// displayFuelCapacity();
+
+// function displayStaffStatus(){
+//    availableAirplanes.forEach(function(element) {
+//       console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff));
+//   });
+// };
+// displayStaffStatus();
+
+// function displaySpeedRangeStatus(){
+//      availableAirplanes.forEach(function(element) {
+//       console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+//   });
+// };
+// displaySpeedRangeStatus()
+
+// >>>> 15/15
