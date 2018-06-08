8f914097f789e8b730b5434ddc7da7b0a2dcf2cf
Fulvio Famularo <fulvio.famularo@nbloc.co>
2018-06-08 16:55:26 +0200

    Req 8/10

--- intro_12_Requests.js
+++ intro_12_Requests.js
@@ -160,3 +160,19 @@
 // ------> picture diagram of the POST scheme
 
 // >>>> 8/10
+const xhr = new XMLHttpRequest;
+const url = 'https://api-to-call.com/endpoint';
+const data = JSON.stringify({id: '200'});
+
+xhr.responseType = 'json';
+
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response;
+  }
+};
+
+xhr.open('POST', url);
+xhr.send(data);
+
+// >>>> 9/10
