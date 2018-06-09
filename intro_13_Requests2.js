6a444c3262edc999367199e29f39e85ead5e277a
Calprimus <fulvio.famularo@hotmail.com>
2018-06-09 16:26:05 +0200

    Req2 4/17

--- intro_13_Requests2.js
+++ intro_13_Requests2.js
@@ -61,13 +61,52 @@
 //   which contains the status of the promise with information the API sent back.
 
 //   >>>> 3/17
-fetch('https://api-to-call.com/endpoint').then(response => {
-    if (response.ok) {
-        return response.json();
-    }
-    throw new Error('Request failed!');
-}, networkError => {
-    console.log(networkError.message)
-}).then(jsonResponse => {
-    return jsonResponse;
-})
+// fetch('https://api-to-call.com/endpoint').then(response => {
+//     if (response.ok) {
+//         return response.json();
+//     }
+//     throw new Error('Request failed!');
+// }, networkError => {
+//     console.log(networkError.message)
+// }).then(jsonResponse => {
+//     return jsonResponse;
+// })
+
+// >>>> 4/17
+// // Information to reach API
+// const url = 'https://api.datamuse.com/words';
+// const queryParams = '?sl='
+
+// // Selects page elements
+// const inputField = document.querySelector('#input');
+// const submit = document.querySelector('#submit');
+// const responseField = document.querySelector('#responseField');
+
+// // AJAX function
+// const getSuggestions = () => {
+//   const wordQuery = inputField.value;
+//   const endpoint = url + queryParams + wordQuery;
+
+//   fetch(endpoint).then(response =>{
+//     if(response.ok){
+//       return response.json();
+//     }
+//   throw new Error('Request failed!');  
+//   }, networkError => {
+//     console.log(networkError.message)
+//   }
+
+//                       ) }
+
+// // Clears previous results and display results to webpage
+// const displaySuggestions = (event) => {
+//   event.preventDefault();
+//   while(responseField.firstChild){
+//     responseField.removeChild(responseField.firstChild);
+//   }
+//   getSuggestions();
+// };
+
+// submit.addEventListener('click', displaySuggestions);
+
+// >>>> 5/17
