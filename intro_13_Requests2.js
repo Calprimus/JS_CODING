ef16ac2a86f495eb9af53fc447340d0be5075a1b
Calprimus <fulvio.famularo@hotmail.com>
2018-06-09 18:03:06 +0200

    Req2 8/17

--- intro_13_Requests2.js
+++ intro_13_Requests2.js
@@ -160,18 +160,54 @@
 // The rest of the request is identical to the GET request.
 
 // >>>> 7/17
-fetch('https://api-to-call.com/endpoint', ({
-    method: 'POST',
-    body: JSON.stringify({ id: '200' })
-})).then(response => {
-    if (response.ok) {
-        return response.json()
-    }
-    throw new Error('Request failed!');
-}, networkError => {
-    console.log(networkError.message)
-}).then(jsonResponse => {
-    return jsonResponse
-})
+// fetch('https://api-to-call.com/endpoint', ({
+//     method: 'POST',
+//     body: JSON.stringify({ id: '200' })
+// })).then(response => {
+//     if (response.ok) {
+//         return response.json()
+//     }
+//     throw new Error('Request failed!');
+// }, networkError => {
+//     console.log(networkError.message)
+// }).then(jsonResponse => {
+//     return jsonResponse
+// })
 
 //   >>>> 8/17
+// Information to reach API
+// const apiKey = '9299153837f044968a04e1cf984eb50b';
+// const url = 'https://api.rebrandly.com/v1/links';
+
+// // Some page elements
+// const inputField = document.querySelector('#input');
+// const shortenButton = document.querySelector('#shorten');
+// const responseField = document.querySelector('#responseField');
+
+// // AJAX functions
+// const shortenUrl = () => {
+//     const urlToShorten = inputField.value;
+//     const data = JSON.stringify({ destination: urlToShorten });
+//     fetch(url, {
+//         method: 'POST',
+//         headers: {
+//             'Content-type': 'application/json',
+//             'apikey': apiKey
+//         },
+//         body: data,
+
+//     })
+// }
+
+// // Clear page and call AJAX functions
+// const displayShortUrl = (event) => {
+//     event.preventDefault();
+//     while (responseField.firstChild) {
+//         responseField.removeChild(responseField.firstChild)
+//     }
+//     shortenUrl();
+// }
+
+// shortenButton.addEventListener('click', displayShortUrl);
+
+// >>>> 9/17
